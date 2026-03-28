620c853874e2c6f29229e27c7ebcde5df5720342
S1lva <diogomcsilva5@gmail.com>
2026-03-28 18:03:57 +0000

    feat: add login_required decorator to protected views

--- users/views/profile.py
+++ users/views/profile.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def profile_view(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-
     profile = request.user.profile_set.first()
     context = {
         'username': request.user.username,
