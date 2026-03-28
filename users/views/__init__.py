87f28abb8eb37820b965e477ad70c064ae7725c8
S1lva <diogomcsilva5@gmail.com>
2026-03-28 17:18:47 +0000

    feat: add profile view with email and password update

--- users/views/__init__.py
+++ users/views/__init__.py
@@ -1,3 +1,5 @@
 from .register import register_view
 from .login import login_view
 from .logout import logout_view
+
+from .profile import profile_view
---
+++ users/views/profile.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import update_session_auth_hash
+
+
+def profile_view(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    profile = request.user.profile_set.first()
+    context = {
+        'username': request.user.username,
+        'email':    request.user.email,
+        'role':     profile.role if profile else '-',
+    }
+
+    if request.method == 'POST':
+        action = request.POST.get('action')
+
+        if action == 'update_profile':
+            email = request.POST.get('email', '').strip()
+            if email:
+                request.user.email = email
+                request.user.save()
+            context['success'] = 'Perfil atualizado!'
+
+        elif action == 'change_password':
+            old_password = request.POST.get('old_password')
+            new_password = request.POST.get('new_password')
+
+            if not request.user.check_password(old_password):
+                context['error_password'] = 'Password antiga incorreta.'
+            else:
+                request.user.set_password(new_password)
+                request.user.save()
+                update_session_auth_hash(request, request.user)
+                context['success_password'] = 'Password alterada com sucesso!'
+
+        context['email'] = request.user.email
+
+    return render(request, 'users/profile.html', context)
